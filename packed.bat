9d8a0903d0b4029efc7dc21461f223d598b9832f
zhangbin-eos <zhangbin.eos@foxmail.com>
2021-10-14 15:02:03 +0800

    chore(packet): 打包脚本等待输入

--- packed.bat
+++ packed.bat
@@ -1,5 +1,6 @@
 @REM @Author: zhangbin.eos@foxmail.com
 @REM @Date:   2021-09-29 10:20:58
 @REM @Last Modified by:   zhangbin.eos@foxmail.com
-@REM Modified time: 2021-09-29 10:21:19
+@REM Modified time: 2021-10-14 15:01:28
 pyinstaller -F ./AutoTest.py
+pause
